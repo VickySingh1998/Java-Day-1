b5022842f29b5e2a92d0de020252c4eeb6dc2f2e
VickySingh1998 <vickysingh8697@gmail.com>
2022-05-29 22:09:27 +0530

    first commit

---
+++ PrimitiveData/src/package2/PrimitiveData.java
@@ -0,0 +1,23 @@
+package package2;
+
+public class PrimitiveData {
+	static int a;
+	static double b;
+	static boolean c;
+	static float d;
+	static char e;
+	static String f;
+	static long g;
+
+	public static void main(String[] args) {
+		System.out.println("The default values of primitive data types are as follows");
+		System.out.println("int=" +a);
+		System.out.println("double=" +b);
+		System.out.println("boolean=" +c);
+		System.out.println("float=" +d);
+		System.out.println("char=" +e);
+		System.out.println("String=" +f);
+		System.out.println("long=" +f);
+	}
+
+}
---
+++ string/src/stringEqual/string.java
@@ -0,0 +1,23 @@
+package stringEqual;
+
+import java.util.Scanner;
+
+public class string {
+
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter First String:");
+		String st1= sc.nextLine();
+		System.out.print("Enter First String:");
+		String st2= sc.nextLine();
+		
+		if (st1==st2) {
+			System.out.print("Enter First String:");
+		else 
+			System.out.print("Enter First String:");
+		}
+		
+		
+	}
+}
